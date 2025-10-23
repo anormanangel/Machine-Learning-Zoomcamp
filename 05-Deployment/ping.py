f96bb12c552ee4cedd8d194dec49d64c5c5956d9
anormanangel <anormanangel@gmail.com>
2025-10-23 19:00:19 +0300

    intro to flask

---
+++ 05-Deployment/ping.py
@@ -0,0 +1,13 @@
+# Web services: introduction to Flask
+
+from flask import Flask
+
+app = Flask('ping')
+
+@app.route('/ping', methods=['GET'])
+
+def ping():
+    return 'PONG'
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
